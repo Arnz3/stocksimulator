b3efcf73d51e127b78061bc80072a3b63a08ee7e
Arnz3 <cuyversarno@gmail.com>
2021-06-29 15:51:08 +0200

    more configurable

--- stocksimulator/stock.py
+++ stocksimulator/stock.py
@@ -7,25 +7,29 @@ class Stock:
         self.name = name
         self.value = 0
         self.climbing = True
+        self.changeValue = (1,5)
+        self.updateRangeX10 = (1,50)
+        self.changeRange = (5,30)
+        self.changeChance = (1,2)
 
     def update(self):
         while True:
             if self.climbing:
-                increment = random.randint(1,5)
+                increment = random.randint(self.changeValue[0],self.changeValue[1])
                 self.value += increment
             else:
-                decrease = random.randint(1,5)
+                decrease = random.randint(self.changeValue[0],self.changeValue [1])
                 if self.value - decrease <= 0:
                     self.value += decrease
                     self.climbing = True
                 else:
                     self.value -= decrease
-            sleep(round(random.randint(1, 50)/10))
+            sleep(round(random.randint(self.updateRangeX10[0], self.updateRangeX10[1])/10))
         
     def changeState(self):
         while True:
-            sleep(random.randint(5, 30))
-            if random.randint(1,2) == 1:
+            sleep(random.randint(self.changeRange[0], self.changeRange[1]))
+            if random.randint(self.changeChance[0],self.changeChance[1]) == 1:
                 self.climbing = True
             else:
                 self.climbing = False
